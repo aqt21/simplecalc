2f0bdb865ebcf1ac7caec9008f161c531c130237
Andrew Tran <andrewtran@Andrews-Mac.local>
2018-01-15 02:54:08 -0800

    Made simplecalc

--- SimpleCalc/SimpleCalc/main.swift
+++ SimpleCalc/SimpleCalc/main.swift
@@ -10,6 +10,76 @@ import Foundation
 
 public class Calculator {
     public func calculate(_ args: [String]) -> Int {
+        if args[args.count - 1] == "count" {
+            return args.count - 1
+        }
+        if args[args.count - 1] == "fact" {
+            if args.count == 1 {
+                return 0
+            }
+            else if args[0] == "1" {
+                return 1
+            }
+            else {
+                var first : Int? = Int(args[args.count - 2])
+                for i in (1 ... first! - 1).reversed() {
+                    first = first! * i
+                }
+                
+                return first!
+            }
+        }
+        if args[args.count - 1] == "avg" {
+            if args.count == 1 {
+                return 0
+            }
+            else if args.count == 2 {
+                let curr : Int? = Int(args[0])
+                return curr!
+            }
+            else {
+                var total = 0
+            
+                for i in 0 ... args.count - 2 {
+                    let curr : Int? = Int(args[i])
+                    total += curr!
+                }
+                return total / (args.count - 1)
+            }
+        }
+        if args[1] == "+" {
+            let first : Int? = Int(args[0])
+            let second : Int? = Int(args[2])
+            print(first! + second!)
+            return first! + second!
+        }
+        if args[1] == "-" {
+            let first : Int? = Int(args[0])
+            let second : Int? = Int(args[2])
+            return first! - second!
+        }
+        if args[1] == "/" {
+            let first : Int? = Int(args[0])
+            let second : Int? = Int(args[2])
+            return first! / second!
+        }
+        if args[1] == "*" {
+            let first : Int? = Int(args[0])
+            let second : Int? = Int(args[2])
+            return first! * second!
+        }
+        if args[1] == "%" {
+            var first : Int? = Int(args[0])
+            let second : Int? = Int(args[2])
+            while first! >= second! {
+                first! -= second!
+                print(first!);
+            }
+            
+            return first!
+        }
+        
+        return 0
     }
     
     public func calculate(_ arg: String) -> Int {
